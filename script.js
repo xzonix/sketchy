124f25a4fa3816acf0d8590a9ed14d1a2490e7e7
onix <112401813+xzonix@users.noreply.github.com>
2024-05-23 19:24:21 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,6 @@ setInterval(()=>{
     counter.innerText = count;
     count++
     
-    if(count > 1) location.replace('https://youtu.be/tgTUtfb0Ok8?si=yI94gi9YReJBHa2D')
+    if(count > 5) location.replace('https://youtu.be/tgTUtfb0Ok8?si=yI94gi9YReJBHa2D')
     
 },1000)
