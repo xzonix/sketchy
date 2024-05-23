6cf3db70dbba4e0ab78dc5d87e882785ae7fa231
onix <112401813+xzonix@users.noreply.github.com>
2024-05-23 19:16:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let counter = document.querySelector('h1');
+let count = 1;
+
+setInterval(()=>{
+
+    counter.innerText = Count;
+    count++
+    
+    if(count > 1) location.replace('https://youtu.be/tgTUtfb0Ok8?si=yI94gi9YReJBHa2D')
+    
+},1000)
